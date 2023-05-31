6d33e88cc74568f337a8b71ea42de0da4f3c6b24
ofrajsingh <rajsingh00seven@gmail.com>
2023-06-01 01:44:03 +0530

    added authorization for routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,20 +2,8 @@ const express= require('express');
 const router= express.Router();
 const Campground= require('../models/campground');
 const catchAsync= require('../utils/catchAsync');
-const ExpressError= require('../utils/ExpressError');
-const {campgroundSchema}= require('../schemas.js');
-const {isLoggedIn}= require('../middleware');
+const {isLoggedIn, isAuthor, validateCampground}= require('../middleware');
 
-const validateCampground= (req,res,next)=>{
-    const {error}= campgroundSchema.validate(req.body);
-    if(error){
-        const msg= error.details.map(el=>el.message).join(',');
-        throw new ExpressError(msg,400);
-    }
-    else{
-        next();
-    }
-}
 
 router.get('/',catchAsync(async (req,res)=>{
     const campgrounds= await Campground.find({});
@@ -28,6 +16,7 @@ router.get('/new',isLoggedIn,(req,res)=>{
 
 router.post('/',isLoggedIn,validateCampground, catchAsync(async (req,res,next)=>{
     const campground= new Campground(req.body.campground);
+    campground.author= req.user._id;
     await campground.save();
     req.flash('success','Succesfully made a new campground');
     res.redirect(`campgrounds/${campground._id}`); 
@@ -35,7 +24,12 @@ router.post('/',isLoggedIn,validateCampground, catchAsync(async (req,res,next)=>
 
 router.get('/:id',catchAsync(async (req,res)=>{
     const {id}= req.params;
-    const campground= await Campground.findById(id).populate('reviews');
+    const campground= await Campground.findById(id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
     if(!campground){
         req.flash('error','Cannot find that campground!');
         return res.redirect('/campgrounds');
@@ -43,7 +37,7 @@ router.get('/:id',catchAsync(async (req,res)=>{
     res.render('campgrounds/show',{campground});
 }));
 
-router.get('/:id/edit',isLoggedIn,catchAsync(async (req,res)=>{
+router.get('/:id/edit',isLoggedIn, isAuthor, catchAsync(async (req,res)=>{
     const {id}= req.params;
     const campground= await Campground.findById(id);
     if(!campground){
@@ -53,14 +47,14 @@ router.get('/:id/edit',isLoggedIn,catchAsync(async (req,res)=>{
     res.render('campgrounds/edit',{campground});
 }));
 
-router.put('/:id',isLoggedIn,validateCampground, catchAsync(async (req,res)=>{
+router.put('/:id',isLoggedIn, isAuthor, validateCampground, catchAsync(async (req,res)=>{
     const {id}= req.params;
     const campground= await Campground.findByIdAndUpdate(id,{...req.body.campground});
     req.flash('success','Succesfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.delete('/:id',isLoggedIn,catchAsync(async (req,res)=>{
+router.delete('/:id',isLoggedIn, isAuthor, catchAsync(async (req,res)=>{
     const {id}= req.params;
     await Campground.findByIdAndDelete(id);
     req.flash('success','Succesfully deleted campground!');
--- seeds/index.js
+++ seeds/index.js
@@ -20,6 +20,7 @@ const seedDB= async ()=>{
         const random1000= Math.floor(Math.random()*1000);
         const price= Math.floor(Math.random()*20)+10
         const camp= new Campground({
+            author: '64761c18d90a767add70430a',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
